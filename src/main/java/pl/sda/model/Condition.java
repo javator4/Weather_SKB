11603c21a11f2158f05af4528baee050a5df5cda
Sonia Borysiak <sonia.borysiak@gmail.com>
2019-07-13 12:37:49 +0200

    weather start

---
+++ src/main/java/pl/sda/model/Condition.java
@@ -0,0 +1,8 @@
+package pl.sda.model;
+
+public class Condition {
+
+    private String text;
+    private String icon;
+    private int code;
+}
---
+++ src/main/java/pl/sda/model/Location.java
@@ -0,0 +1,13 @@
+package pl.sda.model;
+
+public class Location {
+    private String name;
+    private String region;
+    private String country;
+    private float lat;
+    private float lon;
+    private String tz_id;
+    private int localtime_epoch;
+    private String localtime;
+
+}
