f7f67777c356d596eac668a839ac1fef03ae5e61
Sonia Borysiak <sonia.borysiak@gmail.com>
2019-07-20 10:05:56 +0200

    All works folk! log4j update

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -2,7 +2,7 @@ package pl.sda;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.log4j.Logger;
+import org.apache.log4j.Logger;//do komentarzy
 import pl.sda.model.Current;
 import pl.sda.model.Location;
 import pl.sda.model.Weather;
@@ -14,7 +14,7 @@ import java.net.URL;
 
 public class App {
 
-    private static Logger logger= Logger.getLogger(App.class);
+    private static Logger logger = Logger.getLogger(App.class);
 
     public static void main(String[] args) {
         logger.info("Uruchomienie aplikacji: ");
@@ -23,38 +23,17 @@ public class App {
         logger.error("ERROR ");
 
 
-     /*  //String city = "Paris";
-
-
-  //  Current current = weatherService.getCityWeather("Torun");
-
-       // System.out.println("Temperature in " + city + ": " + current.getTemp_c() + "C");
-        //System.out.println("Feels like, feels like it's  " + current.getFeelslike_c() + "C" + " in " + city);
-        //System.out.println("Pressure in  " + city + ": " + current.getPressure_mb());*/
-        WeatherService weatherService = new WeatherService("http://api.apixu.com/v1/current.json",
-                "2ba889217eed469cb8a81157191307");
         String url = "http://api.apixu.com/v1/current.json?key=2ba889217eed469cb8a81157191307&q=Paris";
 
+        WeatherService weatherService = new WeatherService("http://api.apixu.com/v1/current.json",
+                "2ba889217eed469cb8a81157191307");
 
-      //  Current current = weatherService.getJData("Torun").getCityWeather();
 
-    // Location location=weatherService.getJData("Torun").getLocation();
-
-       // System.out.println(current);
-
-      //  System.out.println("Lat: "+location.getLat());
-        //System.out.println("Lon: "+location.getLon());
-
-        WeatherForecast weatherForecast=new OrgImplementation(weatherService,"Torun");
-        WeatherForecast weatherForecast1=new FasterImplementation(weatherService,"Torun");
+        WeatherForecast weatherForecast = new OrgImplementation(weatherService, "Torun");
+        WeatherForecast weatherForecast1 = new FasterImplementation(weatherService, "Torun");
 
         System.out.println(weatherForecast.getWeather());
         System.out.println(weatherForecast1.getWeather());
-
-
-
-
-
 
 
     }
